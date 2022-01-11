67a1610880b1f826accfb89d323bcbc796cd2f79
harmolipi <nikob381@gmail.com>
2022-01-11 12:02:45 -0500

    Added ability to tweet quote and cleaned up

--- src/QuoteCard.js
+++ src/QuoteCard.js
@@ -1,54 +1,67 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import Quote from './Quote';
 
 const QuoteCard = () => {
-  const quoteLink1 =
-    'https://gist.githubusercontent.com/christianvuerings/6624542/raw/379a1943f1881b3d3d4ca3e3f456df4d4608c9e7/quotes.json';
-  const quoteLink2 =
+  const quoteLink =
     'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json';
 
   const [quotes, setQuotes] = useState([]);
+  const [quote, setQuote] = useState({ quote: '', author: '' });
 
   useEffect(() => {
+    const fetchQuotes = async () => {
+      const response = await axios(quoteLink);
+
+      setQuotes(response.data.quotes);
+    };
+
     fetchQuotes();
   }, []);
 
-  const fetchQuotes = async () => {
-    // const response = await fetch(
-    //   'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json'
-    // );
-    // const quotes = await response.json();
-
-    const response = await axios(
-      'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json'
-    );
-
-    setQuotes(response.data.quotes);
+  const setRandomQuote = (e) => {
+    if (e) e.preventDefault();
+    const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
+    setQuote(randomQuote);
   };
 
-  // useEffect(() => {
-  //   fetchQuotes().then((fetchedQuotes) => {
-  //     setQuotes(fetchedQuotes.quotes);
-  //   });
-  // });
+  useEffect(() => {
+    setRandomQuote();
+  }, [quotes]);
 
-  // const [quote, setQuote] = useState('');
-
-  // const getQuote = () => {};
+  const tweetLink = quote
+    ? `https://twitter.com/intent/tweet?hashtags=quotes&text=${quote.quote} - ${quote.author}`
+    : '';
 
   return (
-    <div className="card">
+    <div className="card" id="quote-box">
       <div className="card-header">Quote</div>
       <div className="card-body">
         <blockquote className="blockquote mb-0">
-          <p>{quotes[0]['quote'] || 'No quote found'}</p>
-          <footer className="blockquote-footer">
-            <cite>{quotes[0]['author'] || 'No author found'}</cite>
-          </footer>
+          <Quote quote={quote} />
         </blockquote>
-        <button href="#" className="btn btn-primary mt-4">
-          Get a new quote
-        </button>
+        <div id="buttons" className="d-flex justify-content-between mt-4">
+          <div className="mr-auto">
+            <button
+              onClick={setRandomQuote}
+              className="btn btn-primary"
+              id="new-quote"
+            >
+              Get a new quote
+            </button>
+          </div>
+          <div className="">
+            <a
+              href={tweetLink}
+              target="_blank"
+              rel="noreferrer"
+              id="tweet-quote"
+              className="btn btn-secondary"
+            >
+              Tweet quote
+            </a>
+          </div>
+        </div>
       </div>
     </div>
   );
